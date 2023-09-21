36f6ab4de6ab3dee3814dd9ebf5c3c0405e1699e
Moammar Qaddafi <moammar1998@gmail.com>
2023-09-21 15:11:23 +0500

    Completed the navigation bar and home_page with  mock backend. - integrated the onTap functionality of action buttons. previously   these buttons using the container which do not work well with Inkwell   I replaced it with Ink and added Inkwell to get the button work. - We have co-worker list fully functional with all the requirements and   conditions in case list is empty or hydrated.

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -220,7 +220,7 @@ class _HomePageState extends State<HomePage> {
                           }
                         });
                   }),
-            )
+            ),
           ],
         ),
       ),

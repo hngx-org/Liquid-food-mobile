5692979eba65e3d1a3fc54f16a027f7c25bec201
MrazTevin <millatevin@gmail.com>
2023-09-22 05:15:45 +0300

    feat: add validation and activate password toggle

--- lib/screens/widgets/auth_input_password_container.dart
+++ lib/screens/widgets/auth_input_password_container.dart
@@ -1,45 +1,64 @@
 import 'package:flutter/material.dart';
 
 
-class AuthInputPasswordContainer extends StatelessWidget {
+class AuthInputPasswordContainer extends StatefulWidget {
   const AuthInputPasswordContainer({
-    super.key,
+    Key? key,
     required this.height,
     required this.hintText,
     required this.iconData,
-    required this.controller, this.validator,
-  });
+    required this.controller,
+    this.validator,
+  }) : super(key: key);
 
   final double height;
   final String hintText;
   final IconData iconData;
   final TextEditingController controller;
-  final Function(String value)? validator;
+  final String? Function(String?)? validator;
+
+  @override
+  _AuthInputPasswordContainerState createState() =>
+      _AuthInputPasswordContainerState();
+}
+
+class _AuthInputPasswordContainerState extends State<AuthInputPasswordContainer> {
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: height * 0.060,
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6),
-          border: Border.all(color: const Color(0xFF0085FF)),
-        ),
-        child: TextFormField(
-          
-         
-          obscureText: true,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: hintText,
-            hintStyle: const TextStyle(
-              fontSize: 10,
-              fontWeight: FontWeight.w400,
-              color: Color(0xFFA6A6A6),
-            ),
-            suffixIcon: Icon(iconData),
+      height: widget.height * 0.060,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6),
+        border: Border.all(color: const Color(0xFF0085FF)),
+      ),
+      child: TextFormField(
+        controller: widget.controller,
+        obscureText: !_isPasswordVisible,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            color: Color(0xFFA6A6A6),
           ),
-        ));
+          suffixIcon: IconButton(
+            icon: Icon(
+              _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+            ),
+            onPressed: () {
+              setState(() {
+                _isPasswordVisible = !_isPasswordVisible;
+              });
+            },
+          ),
+        ),
+        validator: widget.validator,
+      ),
+    );
   }
 }

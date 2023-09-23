6120f1d3fcbdd545308d8bd2c42209ae422e7060
N-DiLo <egbekwunwanedilobu@gmail.com>
2023-09-23 07:59:41 +0100

    fix: removed unused import

--- lib/withdrawal/presentation/pages/withdrawal_screen.dart
+++ lib/withdrawal/presentation/pages/withdrawal_screen.dart
@@ -3,7 +3,6 @@ import 'package:iconly/iconly.dart';
 
 import '../../../feature/utils/colors.dart';
 import '../../../feature/utils/icons.dart';
-import '../../presentation/pages/withdraw_account.dart';
 import '../widgets/available_lunches.dart';
 import '../widgets/withdraw_summary.dart';
 import './/withdrawal/presentation/widgets/w_button.dart';
